9f32d24d94a45fec162f863e89ab4585e781ef32
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-05-20 15:01:20 +0430

    Favorite(Controller, routes, services, store)

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ import authRouter from './routes/authRoutes.js';
 import carRouter from './routes/carRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import contactRouter from './routes/contactRoutes.js';
+import favoriteRouter from './routes/favoriteRoutes.js';
 import feedbackRouter from './routes/feedbackRoutes.js';
 import commentRouter from './routes/commentRoutes.js';
 
@@ -91,6 +92,7 @@ app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/cars', carRouter);
 app.use('/api/v1/bookings', bookingRouter);
 app.use('/api/v1/contact', contactRouter);
+app.use('/api/v1/favorites', favoriteRouter);
 app.use('/api/v1/feedback', feedbackRouter);
 app.use('/api/v1/comments', commentRouter);
 
